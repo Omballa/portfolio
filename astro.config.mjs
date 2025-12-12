7fedbca8e68cd86b4f00b765756b07bfa08633bb
Brandon <williamomballa@gmail.com>
2025-12-12 15:39:49 +0300

    Fixing small issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  outDir: 'dist',
   vite: {
     plugins: [tailwindcss()],
   },
